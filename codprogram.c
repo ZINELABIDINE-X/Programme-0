f330696a067170d0dd37e8058fbb963454aabc61
ZINELABIDINE-X <zinelabidinecyber@gmail.com>
2024-09-23 03:12:55 +0100

    first commit

---
+++ codprogram.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/*Programme qui permet de demander un nombre entier à l’utilisateur, et l’informe ensuite si ce nombre */
+
+int main()  {
+    int A;
+
+    printf("Please enter a number : ");
+    scanf("%d",&A);
+
+    if (A >= 0)
+
+        if(A > 0)
+    
+          printf("number is positive");
+         else
+             printf("number is null");
+    
+    else 
+    
+        printf("number is negative");
+    
+    
+    
+        
+    
+    
+    
+}
